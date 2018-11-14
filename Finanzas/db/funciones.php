23b79218a13e5db718d9e75990d64be9e78a8a2d
Yuri <master191285@hotmail.com>
2018-11-14 13:20:53 -0300

    Logica insert id de usuario

--- Finanzas/db/funciones.php
+++ Finanzas/db/funciones.php
@@ -20,6 +20,27 @@
 		
 		}
 
+		function getTipoGasto(){
+
+			try {
+				getcwd();				
+				$db = getDB();
+				$query = "SELECT id, descripcion FROM tipo_gasto";
+				$stmt = $db->prepare($query);
+
+				$stmt->execute();   				
+				var_dump($stmt);
+				echo "<option value='' disabled selected>SELECCIONE TIPO GASTO</option>";			
+				while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+					extract($row);							
+					echo'<OPTION VALUE="'.$row['id'].'">'.strtoupper($row['descripcion']).'</OPTION>';  
+				}    				
+			}catch (Exception $e) {
+				echo '{"error":{"text":'. $e->getMessage() .'}}';
+			}
+		
+		}		
+
 		function insertarIngreso(){			
 
      		try{    
@@ -28,16 +49,18 @@
      			$mensaje = "";
 				$monto=htmlspecialchars(strip_tags($_POST['monto']));
 		        $fecha=htmlspecialchars(strip_tags($_POST['fecha']));
-		        $tipoIngreso=htmlspecialchars(strip_tags($_POST['tipoIngreso']));		        
+		        $tipoIngreso=htmlspecialchars(strip_tags($_POST['tipoIngreso']));
 		        if (strlen(htmlspecialchars(strip_tags($_POST['observaciones']))) == 0) {
 		        	$observaciones = "Sin Observaciones";
 		        }else{
 		        	$observaciones=htmlspecialchars(strip_tags($_POST['observaciones']));
 		        }
+		        $id_usuario=htmlspecialchars(strip_tags($_POST['id_user']));
+		        
 		        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		        $query = "INSERT INTO ingresos (monto,fecha, id_tipo_ingreso, observaciones) values(?, ?, ?, ?)";
+		        $query = "INSERT INTO ingresos (monto,fecha, id_tipo_ingreso, observaciones, id_usuario) values(?, ?, ?, ?, ?)";
 		        $inserccion = $db->prepare($query);
-				$inserccion->execute(array($monto,date($fecha),$tipoIngreso, $observaciones));            
+				$inserccion->execute(array($monto,date($fecha),$tipoIngreso, $observaciones, $id_usuario));            
                  
 		        if($inserccion){		            
 		            $mensaje = "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
@@ -58,5 +81,44 @@
         	die('ERROR: ' . $exception->getMessage());
     		}
     	}
+
+		function insertarGasto(){			
+
+     		try{    
+     			getcwd();  	
+     			$db = getDB();
+     			$mensaje = "";
+				$monto=htmlspecialchars(strip_tags($_POST['monto']));
+		        $fecha=htmlspecialchars(strip_tags($_POST['fecha']));
+		        $tipoGasto=htmlspecialchars(strip_tags($_POST['tipoGasto']));		        
+		        if (strlen(htmlspecialchars(strip_tags($_POST['observaciones']))) == 0) {
+		        	$observaciones = "Sin Observaciones";
+		        }else{
+		        	$observaciones=htmlspecialchars(strip_tags($_POST['observaciones']));
+		        }
+		        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		        $query = "INSERT INTO gastos (monto,fecha, id_tipo_gasto, observaciones) values(?, ?, ?, ?)";
+		        $inserccion = $db->prepare($query);
+				$inserccion->execute(array($monto,date($fecha),$tipoGasto, $observaciones));            
+                 
+		        if($inserccion){		            
+		            $mensaje = "<div class='alert alert-success alert-dismissible fade show' role='alert'>";
+		            $mensaje.= "<strong>Exito!</strong> Registro Insertado.";
+		            $mensaje.= "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+		            $mensaje.= "<span aria-hidden='true'>&times;</span></button></div>";
+		            echo $mensaje;
+		        }else{
+					$mensaje = "<div class='alert alert-danger alert-dismissible fade show' role='alert'>";
+		            $mensaje.= "<strong>Error!</strong> Error al grabar.";
+		            $mensaje.= "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+		            $mensaje.= "<span aria-hidden='true'>&times;</span></button></div>";
+		            echo $mensaje;
+		        }
+         
+    		}     	
+    		catch(PDOException $exception){
+        	die('ERROR: ' . $exception->getMessage());
+    		}
+    	}    	
 
 ?>
